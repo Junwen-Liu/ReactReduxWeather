63a711740f48de98a5433ba781283307e2dd9ffb
Liu <jliu5@HFHS.ORG>
2018-02-01 15:28:54 -0500

    inital commit

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY="2f57b636e10b12fddab1995a38944cd5";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY}`;
+
+//set up single source of action type, and will be use in both action and reducer, easier for debug if there's typo
+export const FETCH_WT = "FETCH_WT";
+
+export function fetchwt(ct){
+  const url=`${ROOT_URL}&q=${ct},us`;
+
+  const request=axios.get(url);
+  
+  //return action as the object, use {}, not()
+  return{
+    type: FETCH_WT,
+    payload: request
+  };
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators} from 'redux';
+import {fetchwt} from '../actions/index';
+
+//gonna turn the input into a controll field
+class Searchbar extends Component{
+  //initialize component state in constructor
+  constructor(props){
+    super(props);
+    
+    this.state={term: ''};
+    
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+  
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+  }
+  
+  onFormSubmit(event){
+    //to tell the browser don;t submit the form
+    event.preventDefault();
+    
+    //we need to go and fetch data
+    this.props.fetchwt(this.state.term);
+    this.setState({term: ''});
+  }
+  
+  render(){
+    return(
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="get a five-day forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+//anything return from this functioon will end up as props on this conatiner
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchwt}, dispatch);
+}
+
+export default connect (null, mapDispatchToProps)(Searchbar);
+
+
+
